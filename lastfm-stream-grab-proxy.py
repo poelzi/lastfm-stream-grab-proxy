fdc8c8ef360b6fd04f0336be3daa59c214635da3
Alexander Else <aelse@else.id.au>
2011-09-21 14:14:06 +1000

    more flexible request handling via sockets

--- lastfm-stream-grab-proxy.py
+++ lastfm-stream-grab-proxy.py
@@ -2,19 +2,88 @@
 
 import SocketServer
 import SimpleHTTPServer
-import urllib
+import socket
+from select import select
+from urlparse import urlparse
+import re
 
 class ProxyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    def foo(self):
-        x = 1
+    def __init__(self, *args):
+        SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, *args)
+        self.protocol = 'HTTP/1.0'
 
-    def do_GET(self):
-        self.copyfile(urllib.urlopen(self.path), self.wfile)
+    def _connect(self, netloc):
+        port = 80
+        separator = netloc.find(':')
+        if separator > 0:
+            port = int(netloc[separator+1:])
+            netloc = netloc[:separator]
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try: sock.connect((netloc, port))
+        except socket.error:
+            return None
+        return sock
+
+    def _do_method(self):
+        (scheme, netloc, path, params, query, fragment) = urlparse(self.path, scheme='http')
+        if scheme != 'http' or fragment or netloc == None:
+            self.send_error(400, 'Invalid URL %s' % self.path)
+            return
+        sock = self._connect(netloc)
+        if sock is not None:
+            self.log_request()
+            if self.headers.has_key('Proxy-Connection'):
+                del self.headers['Proxy-Connection']
+            self.headers['Connection'] = 'close'
+
+            sock.send("%s %s %s\r\n" % (self.command, self.path, self.request_version))
+            for header in self.headers.items():
+                sock.send("%s: %s\r\n" % header)
+            sock.send("\r\n")
+            self._read_write(sock)
+        else:
+            self.send_error(404, "Could not connect to host %s:%d" % (netloc, port))
+
+    do_GET  = _do_method
+    do_HEAD = _do_method
+    do_POST = _do_method
+
+    def _read_write(self, server_sock):
+        rlist = [self.connection, server_sock]
+        wlist = []
+        max_idle = 10
+        idle_count = 0
+        seen_http_header = False
+        while 1:
+            (r, _, x) = select(rlist, wlist, rlist, 1)
+            if x: break
+            idle_count += 1
+            if r:
+                for descriptor in r:
+                    data = descriptor.recv(8192)
+                    if data:
+                        idle_count = 0
+                        if descriptor is server_sock:
+                            # http header is in first server packet
+                            if not seen_http_header:
+                                seen_http_header = True
+                                m = re.match('HTTP/\d\.\d (\d+)', data)
+                                if m: self.log_request(m.groups()[0])
+                                else: self.log_request()
+                            self.connection.send(data)
+                        else:
+                            server_sock.send(data)
+            if idle_count >= max_idle: break
 
 def run_server(addr, port):
-    httpd = SocketServer.ForkingTCPServer((addr, port), ProxyHandler)
+    #httpd = SocketServer.ForkingTCPServer((addr, port), ProxyHandler)
+    httpd = SocketServer.ThreadingTCPServer((addr, port), ProxyHandler)
     print 'Listening on %s:%d' % (addr, port)
-    httpd.serve_forever()
+    try: httpd.serve_forever(poll_interval=0.5)
+    except:
+        print 'Shutting down'
+        httpd.shutdown()
 
 if __name__ == "__main__":
     addr = 'localhost'
